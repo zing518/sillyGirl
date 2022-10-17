ad3faa4308998b611dd92b94132f291590764b3c
cdle <50124752+cdle@users.noreply.github.com>
2022-10-17 11:55:32 +0800

    Update main.go

--- main.go
+++ main.go
@@ -27,42 +27,5 @@ func main() {
 	logs.Info("Http服务已运行(%s)。", sillyGirl.GetString("port", "8080"))
 	go core.Server.Run("0.0.0.0:" + port)
 	logs.Info("命令怎么失效了？关注频道 https://t.me/kczz2021 获取最新消息，发言关注微信公众号送喜官获取暗号。")
-	d := false
-	for _, arg := range os.Args {
-		if arg == "-d" {
-			d = true
-		}
-	}
-	if !d {
-		t := false
-		for _, arg := range os.Args {
-			if arg == "-t" {
-				t = true
-			}
-		}
-		if t {
-			logs.Info("终端交互已启用。")
-			scanner := bufio.NewScanner(os.Stdin)
-			for scanner.Scan() {
-				data := scanner.Text()
-				f := &core.Faker{
-					Type:    "terminal",
-					Message: string(data),
-					Carry:   make(chan string),
-					Admin:   true,
-				}
-				core.Senders <- f
-				go func() {
-					for v := range f.Listen() {
-						fmt.Printf("\x1b[%dm%s \x1b[0m\n", 31, v)
-					}
-				}()
-			}
-			logs.Info("终端交互异常,请检查运行环境设置,如果是docker环境,请附加-it参数")
-		} else {
-			logs.Info("终端交互不可用，运行带-t参数即可启用。")
-		}
-	}
-
 	select {}
 }
