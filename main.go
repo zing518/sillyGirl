ce233b0af29cd85d1980b6e78a38630e66e9edfb
cdle <50124752+cdle@users.noreply.github.com>
2022-10-17 11:57:12 +0800

    Update main.go

--- main.go
+++ main.go
@@ -26,6 +26,5 @@ func main() {
 	}
 	logs.Info("Http服务已运行(%s)。", sillyGirl.GetString("port", "8080"))
 	go core.Server.Run("0.0.0.0:" + port)
-	logs.Info("命令怎么失效了？关注频道 https://t.me/kczz2021 获取最新消息，发言关注微信公众号送喜官获取暗号。")
 	select {}
 }
