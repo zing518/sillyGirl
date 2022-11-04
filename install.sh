0e88a2056a6d38a12ae2de35634f1b6a91d22b27
cdle <798731886@qq.com>
2022-11-04 10:07:28 +0800

    update

--- install.sh
+++ install.sh
@@ -1,1 +1,29 @@
-s=sillyGirl;a=arm64;if [[ $(uname -a | grep "x86_64") != "" ]];then a=amd64;fi ;if [ ! -d $s ];then mkdir $s;fi ;cd $s;curl -o $s http://app.imdraw.com/sillyGirl_linux_$a && chmod 777 $s;cd ..;$(pwd)/$s/$s && $(pwd)/$s/$s -t
+n="sillyGirl"
+s="/usr/local/$n"
+a=arm64
+if [[ $(uname -a | grep "x86_64") != "" ]]; then 
+    a=amd64
+fi ;
+if [ ! -d $s ]; then 
+    mkdir $s
+fi ;
+cd $s;
+rm -rf $n;
+v=`curl https://gitlab.com/cdle/$a/-/raw/main/compile_time.go --silent | tr -cd "[0-9]"`
+d=""
+if [ ${#v} == 13 ]; then
+    d="https://gitlab.com/cdle/${a}/-/raw/main/sillyGirl_linux_${a}_${v}"
+else
+    v=`curl https://raw.githubusercontent.com/cdle/binary/main/compile_time.go --silent | tr -cd "[0-9]"`
+    if [ ${#v} == 13 ]; then
+        d="https://raw.githubusercontent.com/cdle/binary/main/sillyGirl_linux_${a}_${v}"
+    else
+        echo "Sorry，你网不好！"
+        exit
+    fi
+fi
+echo "检测到版本 $v"
+echo "正在从 $d 下载..."
+curl -o $n $d && chmod 777 $n
+echo "已安装到 $n，开始运行..."
+"$s/$n" -t
